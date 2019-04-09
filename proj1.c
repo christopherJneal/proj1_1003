28caf9c79fa1dca292ed0e12e59cfbf06884986c
Christopher James Neal <cneal1222000@gmail.com>
2019-04-09 03:54:46 +0000

    first selection has been built needs to be implemented for a loop if the wrong choice is selected. then need finish first part so the process can be copied to decryption part of menu.
    need to make substitution cipher and marry that with menu.
    
    once all implemented functions and file I/O needs to be created.

--- proj1.c
+++ proj1.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 
-int main()
-{
-    FILE *input;
-    input = fopen("mes_encrpt", "r");
-    while(feof(input) == 0){
-        char characterRead;
-        fscanf(input, "%c", &characterRead);
-        printf("%c", characterRead);
+int main(void){
+    int frstChc, k;
+    printf("Hello welcome to the cipher.\n");
+    printf("Select from following menu.\n");
+    printf("----------------------\n");
+    printf("|(1) For encryption. |\n");
+    printf("|(2) For decryption. |\n");
+    printf("----------------------\n");
+    scanf("%d", &frstChc);
+    if(frstChc == 1){
+        printf("Encryption was selected.\n");
+        printf("Select from following two encryption ciphers:\n");
+        printf("--------------------------\n");
+        printf("|(1) Rotation cipher.    |\n");
+        printf("|(2) Subsitution cipher. |\n");
+        printf("--------------------------\n");
+        if(scndChc == 1){
+            
+        }
+        else if(scndChc == 2){
+            
+        }
+        else{
+            
+        }
     }
+    else if(frstChc == 2){
+        printf("Decryption was selected.\n");
+    }
+    else{
+        printf("Input caused an error, select from given options!\nTry again.\n");
+    }
+   
 }

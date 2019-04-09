6c3fef399c94a6a134c62c0f53cf8160cea90f84
Christopher James Neal <cneal1222000@gmail.com>
2019-04-09 03:46:42 +0000

    Finished encrypter and decrypoter. working making it avaliable by I/O input

--- proj1.c
+++ proj1.c
@@ -1,36 +1,12 @@
 #include <stdio.h>
 
-
 int main()
 {
-    char message[128], x;    //array of data type=char, name=message,size=128 bytes
-    int key = 4, index;
-    printf( "Enter message to encrypt: " ); //message to be encrypted
-    scanf( "%c\n", message );               //stores message in array
-    /*printf( "Enter encryption key: " );      //data encryption key
-    scanf( "%d\n", &key ); */                 //stores integer in adress assigned for key (pointer?)
-    
-    
-    //for loop runs till message has been totally encrypted == till reaches NULL point
-    for( index = 0; message[index] != '\0'; index++ ){
-        x = message[index];             //assigs each index number a datype from array message[]
-        if( x >= 'a' && x <= 'z' ){     //if contidion is between ASCII number 'a'-'z' || 97-122  
-            x = x + key;
-            if( x > 'z' ){              //if the encrypted ASCII number lies outside boundary conditions 'a'-'z'
-                x = x - 'z' + 'a' - 1;  //minuses 26 from encrypted number, should exclude from encryption?
-            }
-            message[index] = x;
-            //printf("%c\n", message[index]);
-        }
-        else if( x >= 'A' && x <= 'Z' ){     //if contidion is between ASCII number 'a'-'z' || 65-90
-            x = x + key;
-            if( x > 'Z' ){                   //if the encrypted ASCII number lies outside boundary conditions 'a'-'z'
-                x = x -'Z' + 'A' - 1;
-            }
-            message[index] = x;
-            //printf("%c\n", message[index]);
-        }
-        printf("%c\n", message[index]);
-        return 0;    
+    FILE *input;
+    input = fopen("mes_encrpt", "r");
+    while(feof(input) == 0){
+        char characterRead;
+        fscanf(input, "%c", &characterRead);
+        printf("%c", characterRead);
     }
 }

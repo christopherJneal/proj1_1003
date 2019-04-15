a774527dc577ba317f5f25e3c0370e225fd59375
Christopher James Neal <cneal1222000@gmail.com>
2019-04-15 04:16:13 +0000

    works but kills program when reaches second switch statement. bug testing needs to be further tested.
    Committing to GITHUB is not working hopefully other versions that i have committed have been done otherwise i will lose my work but I will just save to extra files within che just so i can recover stuff if i need too.

--- proj1.c
+++ proj1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void){
-    int frstChc, k;
+    int frstChc, scndChc1, scndChc2;
     printf("Hello welcome to the cipher.\n");
     printf("Select from following menu.\n");
     printf("----------------------\n");
@@ -9,28 +9,60 @@ int main(void){
     printf("|(2) For decryption. |\n");
     printf("----------------------\n");
     scanf("%d", &frstChc);
-    if(frstChc == 1){
-        printf("Encryption was selected.\n");
-        printf("Select from following two encryption ciphers:\n");
-        printf("--------------------------\n");
-        printf("|(1) Rotation cipher.    |\n");
-        printf("|(2) Subsitution cipher. |\n");
-        printf("--------------------------\n");
-        if(scndChc == 1){
+    switch(frstChc){
+        case 1: frstChc == 1;
+            printf("Encryption was selected.\n");
+            printf("Select from following two encryption ciphers:\n");
+            printf("--------------------------\n");
+            printf("|(1) Rotation cipher.    |\n");
+            printf("|(2) Subsitution cipher. |\n");
+            printf("--------------------------\n");
+            scanf("%d", &scndChc);
+            switch(scndChc1){
+                case 1: scndChc1 == 1;
+                    printf("Encryption using the rotation cipher was selected\n");
+                    /*
+                     * do some stuff to open and use rotation cipher and print to different file.
+                     */
+                    return 0;
+                case 2: scndChc1 == 2;
+                    printf("Encryption using the substitution cipher was selected\n");
+                    /*
+                     * do some stuff to open and use substitution cipher and print to different file
+                     */
+                    return 0;
+                default:
+                    printf("Input caused an error, select from given options!\nTry again.\n");
+                    return 0;
+            }
+        case 2: frstChc == 2;
+            printf("Decryption was selected.\n");
+            printf("Select from following two decryption ciphers:\n");
+            printf("--------------------------\n");
+            printf("|(1) Rotation cipher.    |\n");
+            printf("|(2) Subsitution cipher. |\n");
+            printf("--------------------------\n");
+            scanf("%d", scndChc2);
+            switch(scndChc2){
+                case 1: scndChc2 == 1;
+                    printf("Decryption using the rotation cipher was selected\n");
+                    /*
+                     * do some stuff to open and use rotation cipher and print to different file.
+                     */
+                    return 0;
+                case 2: scndChc2 == 2;
+                    printf("Decryption using the substitution cipher was selected\n");
+                    /*
+                     * do some stuff to open and use substitution cipher and print to different file
+                     */
+                    return 0;
+                default:
+                    printf("Input caused an error, select from given options!\nTry again.\n");
+                    return 0;
+            }
             
-        }
-        else if(scndChc == 2){
-            
-        }
-        else{
-            
-        }
+        default:
+            printf("Input caused an error, select from given options!\nTry again.\n");
+
     }
-    else if(frstChc == 2){
-        printf("Decryption was selected.\n");
-    }
-    else{
-        printf("Input caused an error, select from given options!\nTry again.\n");
-    }
-   
 }

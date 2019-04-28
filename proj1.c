f7160f3bec4fcd503c8a8f167af3f3d130715756
Christopher James Neal <cneal1222000@gmail.com>
2019-04-28 07:29:56 +0000

    re work but decryption process does not work correctly

--- proj1.c
+++ proj1.c
@@ -1,50 +1,75 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
-void encrypt(char inchar){
-    //Changing chars
-    inchar = inchar + 6; //This is a simple substution
-    //print encrypted char
-    printf("%c",inchar);
-}
+char *encryption(char[]);
+void *decryption(char[]);
+char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char key[26];
 
-
-int main(int argc, char* argv[]){
-    FILE *inputFile;
-    char mes_Inpt[80];
-    char *inputPtr = mes_Inpt;
-    char inchar;
-    int i;
-
-    inputFile = fopen("mes_encrpt", "r");
-
-    //check file
-    if (inputFile == '\0'){ 
-        printf("Failed to open\n");
-        return 0;
-    }
-    else{
-        //inputFile is valid so I read a string
-        //and store it in inputBuffer
-        fgets(mes_Inpt, 80, inputFile);
-
-        //pass every character of inputBuffer to encrypt()
-        for(i=0; i < strlen(mes_Inpt); i++){
-            //printf("%c", inputBuffer[i]);
-            inchar = mes_Inpt[i];
-            if( isalpha(inchar) && islower(inchar) ){
-                encrypt(mes_Inpt[i]);
+void main(){
+    int i, key, choice, flag = 0;
+    char *cipher_text, message[255];
+    printf("ENter plain text\n");
+    scanf("%[^\n]", message);
+    decryption(cipher_text);
+    printf("Your plain text message is: %s\n", message);
+    printf("Press 1 for encryption\n Press 2 for decryption\n Press 'o' for exit\n");
+    scanf("%d", &choice);
+    do{
+        switch( choice ){
+            case 1: 
+                cipher_text = encryption(message);
+                flag = 1;
+                break;
+            case 2: 
+                if( flag == 1 ){
+                    decryption(cipher_text);
+                }
+                else{
+                    printf("First perform encryption process\n");
+                    break;
+                }
+            case 0: 
+                break;
+            default:
+                printf("Please enetr valif choice\n");
+                break;
             }
-            else if( isalpha(inchar) && isupper(inchar) ){
-                encrypt(mes_Inpt[i]);
-            }
-            else{
-                void;
-            }
-        }
-
-    }
-    //printf("\n\n");
+    }while( choice != 0 )
     return 0;
 }
+    char *encryption(char cipher_text[]){
+        int i, value, j;
+        printf("Enter the unique key of 26 characters for encryption:\n");
+        scanf("%s", key);
+        printf("Character replaced\n");
+        printf("abcdefghijklmnopqrstuvwxyz\n");
+        printf("||||||||||||||||||||||||||\n");
+        printf("%s\n", key);
+        for( i = 0; i < strlen(cipher_text); i++){
+            for( j = 0; j < 26; j++){
+                if( alpha[j] == cipher_text[i] ){
+                    cipher_text[i] = key[j];
+                    break;
+                }
+            }
+        }
+    printf("Your encrypted message is: %s\n", cipher_text);
+    return cipher_text;
+    }
+    void *decryption(char cipher_text[]){
+        int i, value, j;
+        char cipher[255];
+        strcpy(cipher, cipher_text);
+        printf("Decryption Process\n");
+        for( i = 0; i < strlen(cipher); i++ ){
+            for( j = 0; j < 26; j++ ){
+                if( cipher[i] == key [j] ){
+                    cipher[i] = alpha[j];
+                    break;
+                }
+            }
+        }
+    printf("Decrypted Text %s", cipher_text);
+    return 0;
+    }
